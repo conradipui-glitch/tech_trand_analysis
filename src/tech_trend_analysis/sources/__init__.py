fe0562071e32a95286470fc878454737f87e0859
conradipui-glitch <conradipui@gmail.com>
2026-08-30 02:59:50 +0600

    feat: export GitHub history adapter

--- src/tech_trend_analysis/sources/__init__.py
+++ src/tech_trend_analysis/sources/__init__.py
@@ -1,5 +1,19 @@
 """Provider adapters that normalize external records into Observation objects."""
 
+from .github_history import (
+    GitHubHistoryClient,
+    GitHubHistoryProtocolError,
+    GitHubHistoryQuery,
+    VerifiedGitEvent,
+)
 from .openalex import OpenAlexAdapter, OpenAlexProtocolError, OpenAlexQuery
 
-__all__ = ["OpenAlexAdapter", "OpenAlexProtocolError", "OpenAlexQuery"]
+__all__ = [
+    "GitHubHistoryClient",
+    "GitHubHistoryProtocolError",
+    "GitHubHistoryQuery",
+    "VerifiedGitEvent",
+    "OpenAlexAdapter",
+    "OpenAlexProtocolError",
+    "OpenAlexQuery",
+]
